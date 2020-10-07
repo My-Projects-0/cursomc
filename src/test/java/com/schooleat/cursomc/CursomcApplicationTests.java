6585b866a46c3de93185e4cdc04e65734329c379
Matheus Silva <matheu_lpp@hotmail.com>
2020-10-06 22:39:48 -0300

    Projeto criado

---
+++ src/test/java/com/schooleat/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schooleat.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
