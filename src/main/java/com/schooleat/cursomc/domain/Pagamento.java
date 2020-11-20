749dc0057b6c8fe007b59f855d0ba37a986d4ecd
Matheus Silva <matheu_lpp@hotmail.com>
2020-11-20 13:08:09 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/schooleat/cursomc/domain/Pagamento.java
+++ src/main/java/com/schooleat/cursomc/domain/Pagamento.java
@@ -10,6 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.schooleat.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
@@ -25,6 +26,7 @@ public abstract class Pagamento implements Serializable {
 	private Integer id;
 	private Integer pagamento;
 
+	@JsonBackReference
 	@OneToOne
 	@JoinColumn(name = "id_pedido")
 	@MapsId
